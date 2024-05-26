946d843d5f1c379582e44e899ca65425d8c82125
Heiliger-Paul <137763215+Heiliger-Paul@users.noreply.github.com>
2024-05-26 00:17:45 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let MenuBtn = document.querySelector("#MenuBtn");
+let Navbar = document.querySelector("header .navbar");
+
+window.onscroll = () => {
+    Navbar.classList.remove("active");
+    MenuBtn.classList.remove("fa-times");
+};
+
+MenuBtn.onclick = () => {
+    MenuBtn.classList.toggle("fa-times");
+    Navbar.classList.toggle("active");
+}
+
+//swiper 
+var swiper = new Swiper(".ReviewSlider", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    spaceBetween: 10,
+    speed: 4000,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        1050: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    }
+});
