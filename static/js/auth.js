3e4764f656ec36a12ec22b7f508a5b793efa830a
Heiliger-Paul <137763215+Heiliger-Paul@users.noreply.github.com>
2024-06-28 07:08:41 -0700

    updated-another-one

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,17 +1,17 @@
-const signUp = document.querySelector("#signUp");
-const signIn = document.querySelector("#signIn");
-// when click sign up button
-signUp.addEventListener('click', () => {
-    document.querySelector('.login__form').classList.remove('active')
-    document.querySelector('.register__form').classList.add('active')
-    document.querySelector('.ball').classList.add('register')
-    document.querySelector('.ball').classList.remove('login')
-});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('signUp').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.querySelector('.login__form').classList.remove('active');
+        document.querySelector('.register__form').classList.add('active');
+        document.querySelector('.login__form').style.display = 'none';
+        document.querySelector('.register__form').style.display = 'block';
+    });
 
-// when click sign in button
-signIn.addEventListener('click', () => {
-    document.querySelector('.login__form').classList.add('active')
-    document.querySelector('.register__form').classList.remove('active')
-    document.querySelector('.ball').classList.add('login')
-    document.querySelector('.ball').classList.remove('register')
+    document.getElementById('signIn').addEventListener('click', function (event) {
+        event.preventDefault();
+        document.querySelector('.register__form').classList.remove('active');
+        document.querySelector('.login__form').classList.add('active');
+        document.querySelector('.register__form').style.display = 'none';
+        document.querySelector('.login__form').style.display = 'block';
+    });
 });
